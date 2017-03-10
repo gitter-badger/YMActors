c4bd6784447ea0913186960ef525963d72e3b74e
yarhoslavme <yarhoslavme@127.0.53.53>
2017-03-09 23:18:49 -0500

    License updated.

--- src/main/java/com/yarhoslav/ymactors/core/interfaces/IObservable.java
+++ src/main/java/com/yarhoslav/ymactors/core/interfaces/IObservable.java
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of YMActors project.
+ * Copyright (C) 2017 Yarhoslav ME <yarhoslavme@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.yarhoslav.ymactors.core.interfaces;
 
 /**
@@ -5,7 +22,10 @@ package com.yarhoslav.ymactors.core.interfaces;
  * @author yarhoslavme
  */
 public interface IObservable {
+
     public void addObserver(Object pEvent, Object pMsg, IActorRef pActor);
+
     public void removeObserver(Object pEvent, IActorRef pActor);
+
     public void notifyObservers(Object pEvent);
 }
